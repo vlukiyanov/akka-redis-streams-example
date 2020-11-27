cde347d79aad27b8587b08521507a65418ebdf38
Vladimir Lukiyanov <vladimir.lukiyanov@me.com>
2020-11-27 22:22:37 +0000

    convert to use async API over reactive, for performance

--- src/main/scala/api/RedisStreamsFlow.scala
+++ src/main/scala/api/RedisStreamsFlow.scala
@@ -1,27 +1,34 @@
 package api
 
 import akka.NotUsed
-import akka.stream.scaladsl.Source._
 import akka.stream.scaladsl.Flow
-import io.lettuce.core.api.reactive.RedisReactiveCommands
+import io.lettuce.core.api.async.RedisAsyncCommands
 
 import scala.collection.JavaConverters._
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.concurrent.duration.DurationInt
+import scala.jdk.FutureConverters._
 
 /*
   Create a Flow from a Redis stream, this uses XADD method and returns the
   string.
  */
 object RedisStreamsFlow {
-  def create(redis: RedisReactiveCommands[String, String],
+  def create(redis: RedisAsyncCommands[String, String],
              stream: String): Flow[Map[String, String], String, NotUsed] = {
+    redis.setAutoFlushCommands(false)
     Flow
       .apply[Map[String, String]]
-      .flatMapConcat(
-        elem =>
-          fromPublisher(
-            redis
-              .xadd(stream, elem.asJava)
-        )
-      )
+      .groupedWithin(1000, 1.second)
+      .mapAsync(4) { elems =>
+        {
+          val futures =
+            elems.map(elem => redis.xadd(stream, elem.asJava).asScala)
+          redis.flushCommands()
+          Future.sequence(futures)
+        }
+      }
+      .mapConcat(identity)
   }
 }
